e7e6016ffd59232ba31ef1fcf8859d10d62d4190
MBabken <babken.muradkhanyan@softconstruct.com>
2024-11-15 11:53:38 +0400

    added import

--- android/src/main/kotlin/com/hiennv/flutter_esim/FlutterEsimPlugin.kt
+++ android/src/main/kotlin/com/hiennv/flutter_esim/FlutterEsimPlugin.kt
@@ -23,6 +23,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import java.lang.ref.WeakReference
+import io.flutter.plugin.common.MethodChannel.Result
 
 
 /** FlutterEsimPlugin */
